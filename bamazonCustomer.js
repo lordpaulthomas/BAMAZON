a46f1c90e186602f07fa99298c995997d3ec8c23
Paul Thomas <traitorpt@gmail.com>
2019-09-30 21:29:27 -0700

    created mysql database and connected it to customer.js file.  added all required node modules (inquirer, mysql, colors)  added gitignore file

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+mysql = require("mysql")
+inquirer = require("inquirer")
+colors = require("colors")
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end()
+});
