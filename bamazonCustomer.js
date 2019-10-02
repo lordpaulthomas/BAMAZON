b4b58987d88d93e049089c66228fb2c17b8d20c6
Paul Thomas <traitorpt@gmail.com>
2019-10-01 21:36:04 -0700

    added functions with mysql queries in order to start menu and complete purchase and update database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,8 +10,113 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+function main() {
+  inquirer
+  .prompt([
+    {
+      type: "confirm",
+      message: "Would yo like to make a purchase?",
+      name: "start"
+    }
+  ])
+  .then(function(response){
+    if (response.start){
+      purchaseList();
+    }
+    else {
+      console.log("See you next time!")
+      connection.end();
+    }
+  })
+}
+
+function purchaseList () {
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end()
+  chooseItem();
 });
+}
+
+function chooseItem() {
+  connection.query("SELECT * FROM products", function (error, results) {
+    if (error) throw error;
+    const choicesArray = []
+    for (let i = 0; i < results.length; i++) {
+      choicesArray[i] = results[i].product_name;
+    }
+
+  inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What product would you like to purchase?",
+          choices: choicesArray,
+          name: 'productChoice'
+        }
+      ])
+      .then(function (results, error) {
+        if (error) {return console.log(error) }
+        connection.query("SElECT item_id FROM products WHERE product_name = ?", results.productChoice,
+          function (err, results) {
+            if (err) throw err;
+            purchase(results[0].item_id)
+          }
+          )
+      })
+      .catch(function(error){
+        if(error){console.log(error)}
+      })    
+    }   
+  )}
+
+
+function purchase(id) {
+  inquirer
+    .prompt([
+      {
+        type: 'number',
+        message: 'How many units would you like to buy?',
+        name: 'units'
+      }
+    ])
+    .then(function (response, error) {
+      if (error) {
+        return console.log(error);
+      }
+      var quantity = parseInt(response.units)   
+      connection.query("SELECT * FROM products", function (error, results) {
+        if (error) throw error;
+        for (let i = 0; i < results.length; i++) {
+          if (id === results[i].item_id) {
+            console.log(results[i].product_name);
+            if (quantity > results[i].stock_quantity) {
+              console.log("Insufficient quantity!")
+              main()
+            }
+            else {
+              newQuantity = results[i].stock_quantity - quantity;
+              price = quantity * results[i].price;
+              console.log("Your total purchase is $" + price)
+              fulfillPurchase(id, newQuantity)
+            }
+          }
+        }
+      })
+    })
+    .catch(function (error) {
+      if (error) {
+        return console.log(error)
+      }
+    })
+}
+
+function fulfillPurchase(num, quantity) {
+  connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [quantity, num],
+    function (err, res) {
+      if (err) throw err;
+      console.log("Quantity updated\n")
+      main()
+    }
+  );
+}
+main()
